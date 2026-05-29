e1e7968bea4b17aa7023f8210baa65dd3c0bd3e4
Ram Anuj <ramanuj2k4@gmail.com>
2026-05-29 11:17:00 +0530

    Add IPL matches ipl-71 (RCB vs GT) and ipl-72 (SRH vs RR)

--- backend/server.js
+++ backend/server.js
@@ -345,8 +345,8 @@ const IPL_MATCHES = [
   { id: "ipl-70", team1: "KKR",  team2: "DC",   date: "2026-05-24", time: "19:30", venue: "Kolkata" },
    { id: "ipl-71", team1: "RCB",  team2: "GT",   date: "2026-05-26", time: "19:30", venue: "Dharamsala" },
      { id: "ipl-72", team1: "SRH",  team2: "RR",   date: "2026-05-27", time: "19:30", venue: "New Chandigarh" },
-      // { id: "ipl-73", team1: "RCB",  team2: "GT",   date: "2026-05-26", time: "19:30", venue: "Dharamsala" },
-       //  { id: "ipl-71", team1: "RCB",  team2: "GT",   date: "2026-05-26", time: "19:30", venue: "Dharamsala" },
+     { id: "ipl-73", team1: "RR",  team2: "GT",   date: "2026-05-26", time: "19:30", venue: "New Chandigarh" },
+     { id: "ipl-74", team1: "RCB",  team2: "GT",   date: "2026-05-26", time: "19:30", venue: "Ahemdabaad" },
 
 ];
 
